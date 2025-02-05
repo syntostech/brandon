265d3730dd8f7973ef0d9ceb03777160048e9170
BRANDON <bvasquez@syntos.xyz>
2025-02-05 12:37:47 -0500

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,15 @@
+// utils.js - Utility functions
+function initLastLogin() {
+    const lastLoginElement = document.getElementById('lastLogin');
+    if (lastLoginElement) {
+        const currentDate = new Date();
+        lastLoginElement.textContent = `Last login: ${currentDate.toLocaleString()}`;
+    }
+}
+
+function initCardAnimations() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card, index) => {
+        card.style.animationDelay = `${index * 0.2}s`;
+    });
+}
