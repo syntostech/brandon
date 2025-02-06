bec0f9c6e2b395e234261d7b0a7851b1ac53dd73
BRANDON <bvasquez@syntos.xyz>
2025-02-05 21:10:21 -0500

    update song

--- audioData.js
+++ audioData.js
@@ -6,16 +6,4 @@ const AUDIO_PLAYLIST = [
         audioUrl: "https://www.syntos.xyz/cdn/songs/tambourine.mp3",
         link: "https://www.youtube.com/watch?v=JvygwqECh4g"
     },
-    {
-        title: "Runaway",
-        artist: "Aurora",
-        audioUrl: "https://your-audio-url-2.mp3",
-        link: "https://example.com/song2"
-    },
-    {
-        title: "Blinding Lights",
-        artist: "The Weeknd",
-        audioUrl: "https://your-audio-url-3.mp3",
-        link: "https://example.com/song3"
-    }
 ];
