8a55abcd9c186fa831b836d9c0a075741a07cc1d
BRANDON <bvasquez@syntos.xyz>
2025-02-07 00:50:48 -0500

    Update audioData.js

--- audioData.js
+++ audioData.js
@@ -1,8 +1,8 @@
 // audioData.js - Audio playlist data
 const AUDIO_PLAYLIST = [
     {
-        title: "Mr.Tambourine",
-        artist: "Aurora",
+        title: "Mr.Tambourine (Aurora Cover)",
+        artist: "Bob Dylan",
         audioUrl: "https://www.syntos.xyz/cdn/songs/tambourine.mp3",
         link: "https://www.youtube.com/watch?v=JvygwqECh4g"
     },
