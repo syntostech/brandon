f7718289358153c076bcf4049fa2380f6ba96042
BRANDON <bvasquez@syntos.xyz>
2025-02-12 16:33:08 -0500

    Update audioData.js

--- audioData.js
+++ audioData.js
@@ -1,21 +1,25 @@
-// audioData.js - Audio playlist data
 const AUDIO_PLAYLIST = [
     {
         title: "Mr.Tambourine (AURORA Cover)",
         artist: "Bob Dylan",
         audioUrl: "https://www.syntos.xyz/cdn/songs/tambourine.mp3",
-        link: "https://www.youtube.com/watch?v=JvygwqECh4g"
+        link: "https://www.youtube.com/watch?v=JvygwqECh4g",
+        copyright: false,
     },
     {
         title: "Crossfire",
         artist: "Stephen",
         audioUrl: "https://www.syntos.xyz/cdn/songs/crossfire.mp3",
-        link: "https://www.youtube.com/watch?v=M42EoG1QPnU"
+        link: "https://www.youtube.com/watch?v=M42EoG1QPnU",
+        copyright: true
+        previewDuration: 30
     },
     {
         title: "To be loved ft. AURORA",
         artist: "Askjell",
         audioUrl: "https://www.syntos.xyz/cdn/songs/looved.mp3",
-        link: "https://www.youtube.com/watch?v=h2bjTFdezG8"
+        link: "https://www.youtube.com/watch?v=h2bjTFdezG8",
+        copyright: true
+        previewDuration: 30
     },
 ];
