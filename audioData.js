1ca15e679f2423bfbabb47c781eb3160dc9f7349
BRANDON <bvasquez@syntos.xyz>
2025-02-26 23:11:04 -0500

    Create audioData.js

---
+++ audioData.js
@@ -0,0 +1,14 @@
+// audioData.js - Audio playlist data
+
+// startTime: 98 - option to set where to start
+
+const AUDIO_PLAYLIST = [
+    {
+        title: "Mr.Tambourine (AURORA Cover)",
+        artist: "Bob Dylan",
+        audioUrl: "get/music/tambourine.mp3",
+        link: "https://www.youtube.com/watch?v=JvygwqECh4g",
+        copyright: false,
+        lyricsUrl: "get/data/song329898439248.srt"
+    },
+];
