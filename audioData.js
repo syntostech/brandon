898eb92d84001c71059d3c8952644eb7a8cb42eb
BRANDON <bvasquez@syntos.xyz>
2025-03-04 14:56:15 -0500

    Update audioData.js

--- audioData.js
+++ audioData.js
@@ -4,11 +4,19 @@
 
 const AUDIO_PLAYLIST = [
     {
-        title: "Mr.Tambourine (AURORA Cover)",
-        artist: "Bob Dylan",
+        title: "Mr.Tambourine",
+        artist: "Bob Dylan (Aurora Cover)",
         audioUrl: "get/music/tambourine.mp3",
         link: "https://www.youtube.com/watch?v=JvygwqECh4g",
         copyright: false,
         lyricsUrl: "get/data/song329898439248.srt"
+    },
+    {
+        title: "All My Demons",
+        artist: "Aurora",
+        audioUrl: "get/music/demons.mp3",
+        link: "https://www.youtube.com/watch?v=3voiVRdgfAc&pp=ygUUYWxsIG15IGRlbW9ucyBhdXJvcmE%3D",
+        copyright: false,
+        lyricsUrl: "get/data/song746498439248.srt"
     },
 ];
